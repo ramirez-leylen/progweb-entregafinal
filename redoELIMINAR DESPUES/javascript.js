04095cc907ff963950f26696780a6b339f63ccb8
ramirez-leylen <ramirezleylen@gmail.com>
2025-06-09 20:35:50 -0300

    agregue categorias

--- redoELIMINAR DESPUES/javascript.js
+++
@@ -1,5 +0,0 @@
-function toggleSideMenu(){
-    // var menuButton = document.getElementById("menuButton");
-    var sideMenu = document.getElementById("sideMenu");
-    sideMenu.classList.toggle("hidden");
-}
